6ea871744edd38040be7cb878398e687649f3fe3
Michaela Lehr <michaela.lehr@geildanke.com>
2018-01-17 10:52:07 +0100

    MOD raycaster visibility and furniture position

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-window.addEventListener('click', function () {
-  var mark = document.querySelector('#mark');
-  const content = `<a-entity io3d-furniture="id:aee3e802-1435-4a89-8e8b-d6057dbf3683" position="0 0 0" rotation="0 180 0" shadow></a-entity>`;
-  var el = document.createElement('a-entity');
+let scene = document.querySelector('a-scene');
 
-  AFRAME.scenes[0].appendChild(el);
-  el.innerHTML = content;
-  var position = mark.getAttribute('position');
-  el.setAttribute('position', position);
-});
+function init() {
+  let raycaster = document.querySelector('[ar-raycaster]');
+  let mark = document.querySelector('#intersection');
 
-var raycaster = document.querySelector('[ar-raycaster]');
-var mark = document.querySelector('#mark');
+  raycaster.addEventListener('click', function () {
+    const content = `<a-entity io3d-furniture="id:aee3e802-1435-4a89-8e8b-d6057dbf3683" shadow="cast:true"></a-entity>`;
+    content.setAttribute('position', raycaster.components.cursor.intersection.point);
+    scene.appendChild(content);
+  });
 
-raycaster.addEventListener('raycaster-intersection', function (evt) {
-  mark.setAttribute('color', 'green');
-  mark.setAttribute('position', evt.detail.intersections[0].point);
-});
+  raycaster.addEventListener('raycaster-intersection', function (e) {
+    mark.setAttribute('position', e.detail.intersections[0].point);
+    mark.setAttribute('visible', true);
+  });
 
-raycaster.addEventListener('raycaster-intersection-cleared', function (evt) {
-  mark.setAttribute('color', 'red');
-});
+  raycaster.addEventListener('raycaster-intersection-cleared', function () {
+    mark.setAttribute('visible', false);
+  });
+}
+
+if (scene.hasLoaded) {
+  init();
+} else {
+  scene.addEventListener('loaded', init);
+}

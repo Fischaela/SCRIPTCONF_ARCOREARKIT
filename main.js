e2d53bf79ac09124985570d797b7aef51d334a07
Michaela Lehr <michaela.lehr@geildanke.com>
2018-01-17 10:55:32 +0100

    DEL init function

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
 let scene = document.querySelector('a-scene');
+let raycaster = document.querySelector('[ar-raycaster]');
+let mark = document.querySelector('#intersection');
 
-function init() {
-  let raycaster = document.querySelector('[ar-raycaster]');
-  let mark = document.querySelector('#intersection');
+raycaster.addEventListener('click', function () {
+  const content = `<a-entity io3d-furniture="id:aee3e802-1435-4a89-8e8b-d6057dbf3683" shadow="cast:true"></a-entity>`;
+  content.setAttribute('position', raycaster.components.cursor.intersection.point);
+  scene.appendChild(content);
+});
 
-  raycaster.addEventListener('click', function () {
-    const content = `<a-entity io3d-furniture="id:aee3e802-1435-4a89-8e8b-d6057dbf3683" shadow="cast:true"></a-entity>`;
-    content.setAttribute('position', raycaster.components.cursor.intersection.point);
-    scene.appendChild(content);
-  });
+raycaster.addEventListener('raycaster-intersection', function (e) {
+  mark.setAttribute('position', e.detail.intersections[0].point);
+  mark.setAttribute('visible', true);
+});
 
-  raycaster.addEventListener('raycaster-intersection', function (e) {
-    mark.setAttribute('position', e.detail.intersections[0].point);
-    mark.setAttribute('visible', true);
-  });
-
-  raycaster.addEventListener('raycaster-intersection-cleared', function () {
-    mark.setAttribute('visible', false);
-  });
-}
-
-if (scene.hasLoaded) {
-  init();
-} else {
-  scene.addEventListener('loaded', init);
-}
+raycaster.addEventListener('raycaster-intersection-cleared', function () {
+  mark.setAttribute('visible', false);
+});

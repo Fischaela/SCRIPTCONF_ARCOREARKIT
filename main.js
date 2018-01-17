624cc6e382f7d5c82906ed258bbe3fbb7f8fd117
Michaela Lehr <michaela.lehr@geildanke.com>
2018-01-17 11:21:34 +0100

    MOD debug raycaster

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var scene = document.querySelector('a-scene');
 var raycaster = document.querySelector('[ar-raycaster]');
 var mark = document.querySelector('#intersection');
+var debug = document.querySelector('#debug');
 
 raycaster.addEventListener('click', function () {
   var content = `<a-entity io3d-furniture="id:aee3e802-1435-4a89-8e8b-d6057dbf3683" shadow="cast:true"></a-entity>`;
@@ -9,7 +10,7 @@ raycaster.addEventListener('click', function () {
 });
 
 raycaster.addEventListener('raycaster-intersection', function (e) {
-  document.querySelector('#debug').innerHTML('intersection',  'e', e, 'detail', e.detail, 'intersections', e.detail.intersections, 'point', e.detail.intersections[0].point);
+  debug.innerHTML = 'intersection',  'e', e, 'detail', e.detail, 'intersections', e.detail.intersections, 'point', e.detail.intersections[0].point;
   mark.setAttribute('position', e.detail.intersections[0].point);
   mark.setAttribute('color', 'green');
 });

204fdc370bb5949d6d812b92a6cab7926d900edb
Michaela Lehr <michaela.lehr@geildanke.com>
2018-01-17 12:25:03 +0100

    MOD furnishing

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ var debug = document.querySelector('#debug');
 window.addEventListener('click', function () {
   var content = `<a-entity io3d-furniture="id:aee3e802-1435-4a89-8e8b-d6057dbf3683" shadow="cast:true"></a-entity>`;
   debug.innerHTML = 'CLICK' +  JSON.stringify(raycaster.components.cursor.intersection.point);
-  content.setAttribute('position', raycaster.components.cursor.intersection.point);
-  scene.appendChild(content);
+  var el = document.createElement('a-entity');
+  AFRAME.scenes[0].appendChild(el);
+  el.innerHTML = content;
+  var position = mark.getAttribute('position');
+  el.setAttribute('position', position);
 });
 
 raycaster.addEventListener('raycaster-intersection', function (e) {

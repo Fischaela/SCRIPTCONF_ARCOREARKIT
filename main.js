325c91fd0c892a032c77c7cda962ef9aaa529dd1
Michaela Lehr <michaela.lehr@geildanke.com>
2018-01-17 11:17:36 +0100

    MOD debug raycaster

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let scene = document.querySelector('a-scene');
-let raycaster = document.querySelector('[ar-raycaster]');
-let mark = document.querySelector('#intersection');
+var scene = document.querySelector('a-scene');
+var raycaster = document.querySelector('[ar-raycaster]');
+var mark = document.querySelector('#intersection');
 
 raycaster.addEventListener('click', function () {
-  const content = `<a-entity io3d-furniture="id:aee3e802-1435-4a89-8e8b-d6057dbf3683" shadow="cast:true"></a-entity>`;
+  var content = `<a-entity io3d-furniture="id:aee3e802-1435-4a89-8e8b-d6057dbf3683" shadow="cast:true"></a-entity>`;
   content.setAttribute('position', raycaster.components.cursor.intersection.point);
   scene.appendChild(content);
 });
 
 raycaster.addEventListener('raycaster-intersection', function (e) {
-  window.alert('intersection',  'e', e, 'detail', e.detail, 'intersections', e.detail.intersections, 'point', e.detail.intersections[0].point);
+  document.querySelector('#debug').innerHTML('intersection',  'e', e, 'detail', e.detail, 'intersections', e.detail.intersections, 'point', e.detail.intersections[0].point);
   mark.setAttribute('position', e.detail.intersections[0].point);
   mark.setAttribute('color', 'green');
 });
